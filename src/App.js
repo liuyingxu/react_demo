3bd8c4416f29858c497cc74dee91d6deb6091039
liuyingxu <liuyingxu@t2cloud.net>
2020-07-08 14:50:09 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'
+import Demo from './demo';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Demo></Demo>
       </header>
     </div>
   );
---
+++ src/demo.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class Demo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: ['1', '2', '3']
+    }
+  }
+  render() {
+    return (
+      <div className="demo">
+        <ul>
+          {
+            this.state.list.map((val, index) => (
+              <li key={index}>{val}</li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Demo
